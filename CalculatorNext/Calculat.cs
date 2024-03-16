7c532ef276e1fd6347df54aa4b3f8f1a5f242dc9
Kpoxu4 <kpoxu4111@gmail.com>
2024-03-17 00:42:58 +0300

    Добавьте файлы проекта.

---
+++ CalculatorNext/Calculat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorNext
+{
+    public class Calculat
+    {
+        public static double Arithmetic(double x, double y, char arithmetic)
+        {
+            double result = 0;
+            switch (arithmetic)
+            {
+                case '+':
+                    result = x + y;
+                    break;
+                case '-':
+                    result = x - y;
+                    break;
+                case '*':
+                    result = x * y;
+                    break;
+                case '/':
+                    if (y != 0)
+                        result = x / y;
+                    break;
+            }
+            return result;
+        }
+
+        public static void Info(string info)
+        {
+            Console.WriteLine(info);
+            Console.WriteLine("для продолжение нажмите любую кнопку");
+            Console.WriteLine("для выхода нажмите ESC");
+        }
+    }
+}
---
+++ CalculatorNext/Program.cs
@@ -0,0 +1,66 @@
+﻿using CalculatorNext;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+while (true) 
+{
+    Console.Clear();
+    Console.WriteLine("Калькулятор");
+    string operation = Console.ReadLine();
+
+    if (operation.Contains(' '))
+        operation = string.Join("", operation.Split(' ').ToArray());
+
+    if (operation != "")
+    {
+        if (operation.Contains('.'))
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+        else
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("Ru");
+
+        if (operation[0] == '-' && operation[1] == '-')
+            operation = operation.Substring(2);
+
+        Regex pattern = new Regex(@"([\-]?\d+[\.,]?\d*)([-+*/])([\-]?\d+[\.,]?\d*)", RegexOptions.Compiled);
+        var match = pattern.Match(operation);
+        double result = 0;
+
+        if (match.Success)
+        {
+            var a = double.Parse(match.Groups[1].Value);
+            var b = char.Parse(match.Groups[2].Value);
+            var c = double.Parse(match.Groups[3].Value);
+            if (b == '/' && c == 0)
+                Calculat.Info("на ноль делить нельзя");
+            else
+            {
+                result = Calculat.Arithmetic(a, c, b);
+                Console.Clear();
+                Console.WriteLine($"{a} {b} {c} = {Math.Round(result, 3, MidpointRounding.ToEven)}");
+                Calculat.Info("Успех");
+            }
+        }
+        else
+            Calculat.Info("Ввели не верные значения");
+    }
+    else
+        Calculat.Info("надо бы чет ввести =)");
+        if (Console.ReadKey().Key == ConsoleKey.Escape)
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
